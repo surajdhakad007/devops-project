cd770a62d6a2556cc55a5492fa1610b03c5458dd
surajdhakad007 <surajdhakad80@gmail.com>
2026-03-24 19:26:13 +0530

    Remove emoji from response message

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Project 🚀');
+  res.send('Hello from DevOps Project ');
 });
 
 app.listen(3000, () => {
